11db43a1952110db213f16150ea399bacbd169a1
bulatsubuh@gmail.com <bulatsubuh@gmail.com>
2023-03-26 12:10:42 +0300

    Добавьте файлы проекта.

---
+++ Балансировка дерева. Алгоритм Дея-Стаута-Уорена/Program.cs
@@ -0,0 +1,184 @@
+﻿using System.Text;
+using static System.Net.Mime.MediaTypeNames;
+using System.Drawing;
+using System.IO;
+using System.Diagnostics;
+using System.Linq.Expressions;
+using System.Globalization;
+//using Балансировка_дерева._Алгоритм_Дея_Стаута_Уорена;
+using System.Xml.Linq;
+using Balancingthetree;
+
+namespace Balancingthetree
+{
+    public class Program
+    {
+        public static int[][] Data;
+
+
+        // Создание данных: 50 наборов
+        // от меньшего к большему
+        // первый набор 100000 элементов
+        // последний набор 1000000 элементов
+        // записывается в Data.txt
+        private void CreateData()
+        {
+            var r = new Random();
+
+            var m = new int[50][];
+
+            var k = 100000;
+
+            for (int i = 0; i < 50; i++)
+            {
+                var mas = new int[k];
+
+
+                for (int j = 0; j < k; j++)
+                {
+                    mas[j] = r.Next(1, 1000000);
+                }
+                k += 18000;
+
+                m[i] = mas;
+            }
+
+            var path = "Data.txt";
+
+            using (StreamWriter writer = new StreamWriter(path, false))
+            {
+
+                for (int i = 0; i < 50; i++)
+                {
+                    var text = new StringBuilder();
+                    for (int j = 0; j < m[i].Length; j++)
+                    {
+                        text.Append($"{m[i][j]} ");
+                    }
+
+                    writer.WriteLine(text.ToString());
+                }
+            }
+        }
+
+
+        // Чтение наборов из Data.txt
+        // Запись в переменную Data
+        private void ReadData(string path = "Data.txt")
+        {
+            Data = new int[50][];
+
+            using (StreamReader reader = new StreamReader(path))
+            {
+                for (int i = 0; i < 50; i++)
+                {
+                    string[] line = reader.ReadLine().Split();
+                    int[] ints = new int[line.Length];
+                    for (int j = 0; j < line.Length; j++)
+                    {
+                        if (line[j] == "") break;
+                        ints[j] = int.Parse(line[j]);
+                    }
+
+                    Data[i] = ints;
+                }
+            }
+        }
+
+        //Запись результатов для графика на время в Result.txt
+        private void CountOfElementsAtTime()
+        {
+            ReadData();
+
+            using (StreamWriter st = new StreamWriter("Result.txt"))
+            {
+                for (int i = 0; i < 50; i++)
+                {
+                    var tree = new Tree<int>(Data[i]);
+
+                    Stopwatch sw = new Stopwatch();
+
+                    sw.Start();
+
+                    tree.Rebalance();
+
+                    sw.Stop();
+
+                    var str = $"{sw.ElapsedMilliseconds}\t{Data[i].Length}";
+
+                    st.WriteLine(str);
+                }
+            }
+        }
+
+
+        // Запись результатов для графика итераций в Result2.txt
+        private void CountOfElementsAtIterations()
+        {
+            ReadData();
+
+            using (StreamWriter st = new StreamWriter("Result2.txt"))
+            {
+                for (int i = 0; i < 50; i++)
+                {
+                    var tree = new Tree<int>(Data[i]);
+
+                    tree.Rebalance();
+
+                    var str = $"{tree.iterations}\t{Data[i].Length}";
+
+                    st.WriteLine(str);
+                }
+            }
+        }
+
+        public static void Main()
+        {
+            // Здесь можете проверять методы
+        }
+    }
+}
+
+
+//var a = new Tree<int>();
+
+//a.Add(9);
+//a.Add(7);
+//a.Add(11);
+//a.Add(2);
+//a.Add(2);
+//a.Add(8);
+//a.Add(10);
+//a.Add(25);
+//a.Add(23);
+//a.Add(31);
+//a.Add(15);
+//a.Add(24);
+//a.Add(13);
+//a.Add(17);
+
+//a.Rebalance();
+
+
+//using (StreamWriter st = new StreamWriter("iters.txt", false))
+//{
+//    for (int i = 0; i < 50; i++)
+//    {
+//        var tree = new Tree<int>(Data[i]);
+
+//        Stopwatch stopwatch = new Stopwatch();
+
+//        stopwatch.Start();
+
+//        tree.Rebalance();
+
+//        stopwatch.Stop();
+
+//        var time = stopwatch.ElapsedMilliseconds;
+
+//        var str = $"{time}\t{Data[i].Length}";
+
+
+//        st.WriteLine(str);
+//    }
+//}
---
+++ Балансировка дерева. Алгоритм Дея-Стаута-Уорена/Tree.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Balancingthetree
+{
+    public class Knot<T> where T : IComparable<T>
+    {
+        public T Value;
+        public Knot<T> Left;
+        public Knot<T> Right;
+    }
+
+    public class Tree<T> where T : IComparable<T>
+    {
+        public Knot<T> Root;
+        public int Count = 0;
+        public int Depth = 0;
+
+        public int iterations = 0;
+
+        public Tree() { }
+
+        public Tree(T[] ms)
+        {
+            CreateTree(ms);
+        }
+
+
+        // Создание дерева по массиву
+        private void CreateTree(T[] ms)
+        {
+            for (int i = 0; i < ms.Length; i++)
+            {
+                Add(ms[i]);
+            }
+        }
+
+
+        private void CreateTree(T item)
+        {
+            Root = new Knot<T>() { Value = item, Left = null, Right = null};
+            Count++;
+            Depth++;
+        }
+
+        // Добавление элемента в дерево рекурсивно
+        public void Add(T item)
+        {
+            if (Root == null)
+            {
+                CreateTree(item);
+                return;
+            }
+
+            AddRecursion(Root, item);
+            Count++;
+        }
+
+        private void AddRecursion(Knot<T> knot, T item, int curDepth = 1)
+        {
+            if (knot.Value.CompareTo(item) == 1)
+            {
+                if (knot.Left == null)
+                {
+                    knot.Left = new Knot<T>() { Value = item, Right = null, Left = null };
+
+                    if (curDepth + 1 >= Depth)
+                    {
+                        Depth = curDepth + 1;
+                    }
+
+                    return;
+                }
+
+                AddRecursion(knot.Left, item, ++curDepth);
+            }
+            else
+            {
+                if (knot.Right == null)
+                {
+                    knot.Right = new Knot<T>() { Value = item };
+
+                    if (curDepth + 1 >= Depth)
+                    {
+                        Depth = curDepth + 1;
+                    }
+
+                    return;
+                }
+
+                AddRecursion(knot.Right, item, ++curDepth);
+            }
+        }
+
+
+        // Алгоритм Дея-Стаута-Уоррена
+        public void Rebalance()
+        {
+            // превращает из дерева в сортированный список
+            TreeToVine();
+            // делает из списка сбалансированное дерево
+            VineToTree();
+        }
+
+
+        public void VineToTree()
+        {
+            int h = (int)Math.Ceiling(Math.Log2(Count));
+            VineToBalancedTree(Root, 0, Count, h);
+            Depth = h;
+        }
+
+        private void VineToBalancedTree(Knot<T> knot, int l, int r, int h)
+        {
+            if (knot == null) return;
+
+            for (int i = 1; knot.Right != null; i++)
+            {
+                if (r == h - 1) return;
+                r--;
+                RotateLeft(ref knot);
+                if (knot.Right == null) break;
+                knot = knot.Right;
+                iterations++;
+            }
+
+            iterations++;
+            if (l < r + 1) VineToBalancedTree(Root, l + 1, r, h);
+        }
+
+        private void TreeToVine()
+        {
+            Knot<T> knot = Root;
+
+            while (knot != null)
+            {
+                while (knot.Left != null)
+                {
+                    RotateRight(ref knot);
+                    iterations++;
+                }
+                knot = knot.Right;
+                iterations++;
+            }
+
+            Depth = Count;
+        }
+
+        private void RotateRight(ref Knot<T> parent)
+        {
+            var pivot = parent.Left;
+            var v = parent.Value;
+            parent.Value = pivot.Value;
+            parent.Left = pivot.Left;
+            var r = pivot.Right;
+            pivot.Right = parent.Right;
+            parent.Right = pivot;
+            pivot.Value = v;
+            pivot.Left = r;
+        }
+
+        private void RotateLeft(ref Knot<T> parent)
+        {
+            var pivot = parent.Right;
+            var v = parent.Value;
+            parent.Value = pivot.Value;
+            parent.Right = pivot.Right;
+            var r = pivot.Left;
+            pivot.Left = parent.Left;
+            parent.Left = pivot;
+            pivot.Value = v;
+            pivot.Right = r;
+        }
+
+        public T GetMax(Knot<T> knot)
+        {
+            if (knot.Right == null) return knot.Value;
+            else return GetMax(knot.Right);
+        }
+    }
+}
